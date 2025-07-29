e76d7f06c8d12ab39d020906e782429bfbd41d5b
safrinDoodleblue <safrin.doodleblue@gmail.com>
2025-07-29 13:21:00 +0530

    messages added

--- Notes/notesController.js
+++ Notes/notesController.js
@@ -13,7 +13,7 @@ exports.createNote = async (req, res) => {
   try {
     const { title, content } = req.body;
     const note = await noteService.createNote(req.user.id, title, content);
-    res.status(201).json(note);
+    res.status(201).json({message: 'Note created',note});
   } catch (err) {
     res.status(500).json({ message: 'Failed to create note' });
   }
@@ -27,7 +27,7 @@ exports.updateNote = async (req, res) => {
       req.body
     );
     if (!updatedNote) return res.status(404).json({ message: 'Note not found' });
-    res.json(updatedNote);
+    res.status(201).json({message: 'Note updated',updatedNote});
   } catch (err) {
     res.status(500).json({ message: 'Failed to update note' });
   }
@@ -37,7 +37,7 @@ exports.deleteNote = async (req, res) => {
   try {
     const deletedNote = await noteService.deleteNote(req.user.id, req.params.id);
     if (!deletedNote) return res.status(404).json({ message: 'Note not found' });
-    res.json({ message: 'Note deleted' });
+    res.status(201).json({ message: 'Note deleted' });
   } catch (err) {
     res.status(500).json({ message: 'Failed to delete note' });
   }
